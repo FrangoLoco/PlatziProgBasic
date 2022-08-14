e00f8b66f2cae421494f2b30d627bf73d85c7b54
FrangoLoco <92340909+FrangoLoco@users.noreply.github.com>
2022-08-14 00:23:19 -0600

    Updated Mokepon.js
    add enemy's atacks

--- PlatziCursoBasico/js/Mokepon.js
+++ PlatziCursoBasico/js/Mokepon.js
@@ -1,7 +1,10 @@
 let ataqueJugador
+let ataqueEnemigo
+
 function iniciarJuego() {
     let botonMascotaJugador = document.getElementById('boton-mascota')
     botonMascotaJugador.addEventListener('click', seleccionarMascotaJugador)
+
     let botonFuego = document.getElementById('boton-fuego')
     botonFuego.addEventListener('click', ataqueFuego)
     let botonAgua = document.getElementById('boton-agua')
@@ -9,30 +12,66 @@ function iniciarJuego() {
     let botonTierra = document.getElementById('boton-tierra')
     botonTierra.addEventListener('click', ataqueTierra)
 }
+
 function seleccionarMascotaJugador() {
     let inputHipodoge = document.getElementById('hipodoge')
     let inputCapipepo = document.getElementById('capipepo')
     let inputRatigueya = document.getElementById('ratigueya')
     let spanMascotaJugador = document.getElementById('mascota-jugador')
-    if (inputHipodoge.checked) { spanMascotaJugador.innerHTML = 'Hipodoge' } else if (inputCapipepo.checked) { spanMascotaJugador.innerHTML = 'Capipepo' } else if (inputRatigueya.checked) { spanMascotaJugador.innerHTML = 'Ratigueya' } else { alert('Selecciona una mascota') }
+    
+    if (inputHipodoge.checked) {
+        spanMascotaJugador.innerHTML = 'Hipodoge'
+    } else if (inputCapipepo.checked) {
+        spanMascotaJugador.innerHTML = 'Capipepo'
+    } else if (inputRatigueya.checked) {
+        spanMascotaJugador.innerHTML = 'Ratigueya'
+    } else {
+        alert('Selecciona una mascota')
+    }
+
     seleccionarMascotaEnemigo()
 }
+
 function seleccionarMascotaEnemigo() {
-    let ataqueAleatorio = aleatorio(1, 3)
+    let mascotaAleatoria = aleatorio(1,3)
     let spanMascotaEnemigo = document.getElementById('mascota-enemigo')
-    if (ataqueAleatorio == 1) { spanMascotaEnemigo.innerHTML = 'Hipodoge' } else if (ataqueAleatorio == 2) { spanMascotaEnemigo.innerHTML = 'Capipepo' } else { spanMascotaEnemigo.innerHTML = 'Ratigueya' }
+
+    if (mascotaAleatoria == 1) {
+        spanMascotaEnemigo.innerHTML = 'Hipodoge'
+    } else if (mascotaAleatoria == 2) {
+        spanMascotaEnemigo.innerHTML = 'Capipepo'
+    } else {
+        spanMascotaEnemigo.innerHTML = 'Ratigueya'
+    }
 }
+
 function ataqueFuego() {
     ataqueJugador = 'FUEGO'
-    alert(ataqueJugador)
+    ataqueAleatorioEnemigo()
 }
 function ataqueAgua() {
     ataqueJugador = 'AGUA'
-    alert(ataqueJugador)
+    ataqueAleatorioEnemigo()
 }
 function ataqueTierra() {
     ataqueJugador = 'TIERRA'
-    alert(ataqueJugador)
+    ataqueAleatorioEnemigo()
 }
-function aleatorio(min, max) { return Math.floor(Math.random() * (max - min + 1) + min) }
+
+function ataqueAleatorioEnemigo() {
+    let ataqueAleatorio = aleatorio(1,3)
+    
+    if (ataqueAleatorio == 1) {
+        ataqueEnemigo = 'FUEGO'
+    } else if (ataqueAleatorio == 2) {
+        ataqueEnemigo = 'AGUA'
+    } else {
+        ataqueEnemigo = 'TIERRA'
+    }
+}
+
+function aleatorio(min, max) {
+    return Math.floor(Math.random() * (max - min + 1) + min)
+}
+
 window.addEventListener('load', iniciarJuego)
